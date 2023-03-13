95d78262cb659422b1ec19c84ee1289ee90e1ed4
joshalbert2433 <joshua.albert.villanueva@gmail.com>
2023-03-13 10:13:12 +0800

    add dark theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,37 @@
-import React from "react";
-import { FiMoon } from "react-icons/fi";
+import React, { useEffect, useState } from "react";
+import { HiOutlineMoon, HiMoon } from "react-icons/hi";
+import { themeChange } from "theme-change";
 
 function Header() {
+	const [isDarkTheme, setIsDarkTheme] = useState(() => {
+		return localStorage.getItem("theme") === "myDark" ? true : false;
+	});
+
+	useEffect(() => {
+		themeChange(false);
+		// 👆 false parameter is required for react project
+
+		console.log(isDarkTheme);
+	}, []);
+
 	return (
 		<div className="h-[10vh] bg-primary flex justify-between items-center px-4 md:px-16">
 			<div className="font-bold md:text-xl">Where in the world?</div>
-			<div className="flex gap-4 items-center">
+			<button
+				className="flex gap-4 items-center"
+				data-toggle-theme="myDark,myLight"
+				data-act-class="ACTIVECLASS"
+				onClick={() => setIsDarkTheme(!isDarkTheme)}
+			>
 				<div>
-					<FiMoon size={20} />
+					{isDarkTheme === false ? (
+						<HiOutlineMoon size={20} />
+					) : (
+						<HiMoon size={20} />
+					)}
 				</div>
 				<div className="font-semibold">Dark Mode</div>
-			</div>
+			</button>
 		</div>
 	);
 }
